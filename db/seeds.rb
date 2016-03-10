10ccaf5b4a783d36bc7775bf295aaaed9504133d
A_Lippok <alippok@hotmail.co.uk>
2016-03-10 16:43:22 +0000

    Adds methods to create new artist, store in database and delete all artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ require_relative('../models/artist.rb')
 require_relative('../models/album.rb')
 require_relative('../models/stock.rb')
 
+Artist.delete_all
+
+@artist = Artist.create( 'name' => 'Metallica')
 
 
 
--- models/artist.rb
+++ models/artist.rb
@@ -9,7 +9,26 @@ class Artist
     @id = nil || params['id']
   end
 
-  
+  def save
+    sql = "INSERT INTO artists(
+    name) VALUES(
+    '#{@name}')"
+    SqlRunner.execute(sql)
+  end
+
+
+  def self.create(params)
+    @artist = Artist.new(params)
+    @artist.save
+  end
+
+
+
+
+  def self.delete_all
+    sql = "DELETE from artists"
+    SqlRunner.execute(sql)
+  end
 
 
 
