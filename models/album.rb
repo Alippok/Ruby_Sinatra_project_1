23fcddeda7e472f45450ae471eb8489ad809e03d
A_Lippok <alippok@hotmail.co.uk>
2016-03-11 08:44:12 +0000

    Begins work on album model. Passes basic return own title method

--- models/album.rb
+++ models/album.rb
@@ -2,13 +2,16 @@ require_relative('../db/sql_runner.rb')
 
 class Album
 
-  attr_reader()
+  attr_reader(:title, :artist_id, :buy_price, :sell_price)
 
-  def initialize
-
-
+  def initialize(params)
+    @title = params['title']
+    @artist_id = params['artist_id']
+    @buy_price = params['buy_price']
+    @sell_price = params['sell_price']
   end
 
+ 
 
 
 
--- specs/album_spec.rb
+++ specs/album_spec.rb
@@ -7,10 +7,13 @@ class TestAlbum < MiniTest::Test
 
 
   def setup
+    @album1 = Album.new( 'title'=>'Kill Em All','artist_id'=>1,'buy_price'=>5, 'sell_price'=>10 )
 
   end
 
-
+  def test_title
+    assert_equal('Kill Em All', @album1.title)
+  end
 
 
 
