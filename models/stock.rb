10485acd28558fdfc2f07d4b4e05a68ff7c5050a
A_Lippok <alippok@hotmail.co.uk>
2016-03-11 11:33:29 +0000

    Checks stock model attr readers are working

--- models/stock.rb
+++ models/stock.rb
@@ -2,13 +2,15 @@ require_relative('../db/sql_runner.rb')
 
 class Stock
 
-  attr_reader()
+  attr_reader(:artist_id, :album_id, :quantity)
 
-  def initialize
-
-
+  def initialize(params)
+    @artist_id = params['artist_id']
+    @album_id = params['album_id']
+    @quantity = 0
   end
 
+  
 
 
 
--- specs/stock_spec.rb
+++ specs/stock_spec.rb
@@ -7,9 +7,12 @@ class TestStock < MiniTest::Test
 
 
   def setup
-
+    @stock1 = Stock.new('artist_id'=> 3, 'album_id'=> 7)
   end
 
+  def test_quantity
+    assert_equal(0, @stock1.quantity)
+  end
 
 
 
