f2a59033ea7d482714343bb8fe17a11b553f7474
A_Lippok <alippok@hotmail.co.uk>
2016-03-11 13:13:42 +0000

    Fixes error in find method

--- models/album.rb
+++ models/album.rb
@@ -2,7 +2,7 @@ require_relative('../db/sql_runner.rb')
 
 class Album
 
-  attr_reader(:title, :artist_id, :buy_price, :sell_price)
+  attr_reader(:id, :title, :artist_id, :buy_price, :sell_price)
 
   def initialize(params)
     @id = nil || params['id']
@@ -30,6 +30,27 @@ class Album
   def self.last_entry
     sql = "SELECT * FROM albums ORDER BY id DESC limit 1"
     SqlRunner.execute(sql).first
+  end
+
+
+  def self.all
+    sql = "SELECT * FROM albums"
+    albums = SqlRunner.execute(sql)
+    return Album.map_items(albums)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM albums WHERE id =#{id}"
+    album = SqlRunner.execute(sql)
+    return Album.map_item(album)
+  end
+  def self.map_items(object)
+    return object.map{|album| Album.new(album)}  
+  end
+
+  def self.map_item(object)
+    album = Album.map_items(object)
+    return album.first
   end
 
   def self.delete_all
--- models/artist.rb
+++ models/artist.rb
@@ -19,11 +19,34 @@ class Artist
     SqlRunner.execute(sql)
     return Artist.new(Artist.last_entry)
   end
+
+  def self.all
+    sql = "SELECT * FROM artists"
+    artists = SqlRunner.execute(sql)
+    return Artist.map_items(artists)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM artists WHERE id = #{id}"
+    artist = SqlRunner.execute(sql)
+    return Artist.map_item(artist)
+  end
   
   def self.last_entry
     sql = "SELECT * from artists ORDER BY id DESC limit 1;"
     SqlRunner.execute(sql).first
   end
+
+  def self.map_items(object)
+    return object.map{|artist| Artist.new(artist)}  
+  end
+
+  def self.map_item(object)
+    artist = Artist.map_items(object)
+    return artist.first
+  end
+
+
 
 
   def self.delete_all
