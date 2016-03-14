d28b979347b659cce369510a0799ac49a2b935a3
A_Lippok <alippok@hotmail.co.uk>
2016-03-14 11:26:01 +0000

    Adds create and new routes for artist

--- controllers/artist.rb
+++ controllers/artist.rb
@@ -5,3 +5,22 @@ get '/artists' do
   @artists = Artist.all
   erb(:"artist/index")
 end
+
+get '/artists/new' do
+  erb(:"artist/new")
+end
+
+post '/artists' do
+  Artist.create(params)
+  redirect to '/artists'
+end
+
+
+
+#create
+
+#edit
+
+#update
+
+#destroy
