9ca0931bbaf7d8bdd9de7cd1299d76c5dabb0205
A_Lippok <alippok@hotmail.co.uk>
2016-03-15 13:48:03 +0000

    Adds extra param to constructor on Stockviews model

--- models/stock_view.rb
+++ models/stock_view.rb
@@ -1,15 +1,16 @@
 class StockView
 
-attr_reader(:album_title, :artist_name, :quantity)
+attr_reader(:album_id, :album_title, :artist_name, :quantity)
 
-  def initialize(title, name, quantity)
+  def initialize(id, title, name, quantity)
+    @album_id = nil || id
     @album_title = nil || title
     @artist_name = nil || name
     @quantity = nil || quantity
   end
 
   def self.map_items(object)
-   result = object.map{|stock_object| StockView.new(stock_object['title'], stock_object['name'], stock_object['quantity'])}
+   result = object.map{|stock_object| StockView.new(stock_object['album_id'], stock_object['title'], stock_object['name'], stock_object['quantity'])}
    return result
   end
 
